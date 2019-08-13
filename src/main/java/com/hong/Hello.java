f8ab6f4e40777a9a0ff8ce144e383c962af156f3
root <root>
2019-08-13 23:44:55 +0800

    test

---
+++ src/main/java/com/hong/Hello.java
@@ -0,0 +1,11 @@
+package com.hong;
+
+/**
+ * @author wanghong
+ * @date 2019/08/13 23:43
+ **/
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello Gradle");
+    }
+}
